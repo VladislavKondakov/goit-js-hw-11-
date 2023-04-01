92ad5124e1b39003f6c6c5aae6ad5fef5cbed74a
VladislavKondakov <agentvl@ukr.net>
2023-04-02 01:36:01 +0200

    u

---
+++ src/js/api-service.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    // cделал 39 потому что так красивее, я знаю что в задании 40.
+    this.PER_PAGE = 39;
+  }
+  async fetchGallery() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        // ключ с сайта pixabay
+        key: '34863112-e4eaf59f6cac1684aac16b51e',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
+
+      const data = response.data;
+
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
